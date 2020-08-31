edea242a094955f42dd20464a809ea46a0bf3ec4
Mdshaffat <shaffatbd@gmail.com>
2020-08-31 12:34:50 +0600

    Add a database commection String in jason file

--- AspDotNetDoc/AspDotNetDoc/Data/MvcMovieContext.cs
+++ AspDotNetDoc/AspDotNetDoc/Data/MvcMovieContext.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.EntityFrameworkCore;
+using AspDotNetDoc.Models;
 namespace AspDotNetDoc.Data
 {
-	public class MvcMovieContext
+	public class MvcMovieContext : DbContext
 	{
+		public MvcMovieContext(DbContextOptions<MvcMovieContext> option)
+			: base(option)
+		{
+
+		}
+		public DbSet<Movie> Movie { get; set; }
+
+
 	}
 }

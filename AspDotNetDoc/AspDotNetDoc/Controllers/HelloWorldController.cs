598be2d5175e2e7b8e5106ade2ee25e49f77a0c8
Mdshaffat <shaffatbd@gmail.com>
2020-08-31 11:18:55 +0600

    Passing Data from the Controller To View --GO-- http://localhost:58811/HelloWorld/Welcome?name=shaffat&numTimes=4

--- AspDotNetDoc/AspDotNetDoc/Controllers/HelloWorldController.cs
+++ AspDotNetDoc/AspDotNetDoc/Controllers/HelloWorldController.cs
@@ -14,9 +14,11 @@ namespace AspDotNetDoc.Controllers
 			return View();
 		}
 
-		public string Welcome(string name, int numTimes = 1)
+		public IActionResult Welcome(string name, int numTimes = 1)
 		{
-			return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+			ViewData["Message"] = "Hello" + name;
+			ViewData["NumTimes"] = numTimes;
+			return View();
 		}
 
 

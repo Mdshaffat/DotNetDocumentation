02841c7d762df5a2c8639f314ecab2e6ba7be9e3
Mdshaffat <shaffatbd@gmail.com>
2020-09-05 09:57:23 +0600

    right

--- MvcMovie/Data/MvcMovieContext.cs
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -1,25 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
 
 namespace MvcMovie.Data
 {
-    public class MvcMovieContext
+    public class MvcMovieContext : DbContext
     {
-        public class MvcMovieContext : DbContext
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
         {
-            public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
-                :base(options)
-            {
-
-            }
-
-            public DbSet<Movie> Movie { get; set; }
-
         }
+
+        public DbSet<Movie> Movie { get; set; }
     }
 }
